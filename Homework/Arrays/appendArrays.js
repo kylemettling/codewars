15a07287cf5840652db042c10d55c6b5d845b4e3
WON-DER <wonderfilledyt@gmail.com>
2021-03-13 19:14:13 -0800

    added appendArrays.js

---
+++ Homework/Arrays/appendArrays.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53a8a476947277a3020001cc/train/javascript
+
+function appendArrays(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+appendArrays([1, 2], [2, 4]);
+appendArrays([1, 2], [3, 4]);
+appendArrays(["this"], ["that"]);
+appendArrays(["a", "B"], ["c", "D"]);
+appendArrays([1, 2], [1]);
