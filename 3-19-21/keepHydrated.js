cbf6dd7c38876a250505ed53fc0bd83b847c9988
kylemettling <58491427+kylemettling@users.noreply.github.com>
2021-03-19 13:40:28 -0700

    Add files via upload
    Added keepHydrated.js Codewars challenge

---
+++ 3-19-21/keepHydrated.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+function litres(time) {
+    // return 0;
+    // return Math.trunc(time)
+    console.log(Math.trunc(time* .5))
+  }
+
+
+
+
+
+
+litres(2)
+litres(1.4)
+litres(12.3)
+litres(0.82)
+litres(11.8)
+litres(1787)
+litres(0)
