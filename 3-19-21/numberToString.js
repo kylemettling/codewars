a555c0efa25b3ae4c6c3f3d6f468c2e96b4f617c
kylemettling <58491427+kylemettling@users.noreply.github.com>
2021-03-19 11:22:11 -0700

    Codewars for March 19th 2021
    Added numberToString.js

---
+++ 3-19-21/numberToString.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+function numberToString(num) {
+    console.log(String(num));
+  }
+
+
+
+
+numberToString(67)
+numberToString(101)
+numberToString(56)
+numberToString(12030)
