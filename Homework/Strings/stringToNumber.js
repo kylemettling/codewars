928307d7b29e4dc133cb3277e10912acaf6872e7
WON-DER <wonderfilledyt@gmail.com>
2021-04-24 11:26:28 -0700

    Second String Codewars challenge

---
+++ Homework/Strings/stringToNumber.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+var stringToNumber = function (str) {
+  return Number(str);
+};
+
+stringToNumber("1234");
+stringToNumber("605");
+stringToNumber("1405");
+stringToNumber("-7");
