06eee4cf0e4bb25a4b7a14eeb8a8e99634b78541
WON-DER <wonderfilledyt@gmail.com>
2021-06-09 21:04:17 -0700

    countDevelopers.js

---
+++ 6-16-21/countDevelopers.js
@@ -0,0 +1,62 @@
+// https://www.codewars.com/kata/582746fa14b3892727000c4f/train/javascript
+
+function countDevelopers(list) {
+  return list.filter((a) => a.continent === "Europe").length;
+}
+
+var list1 = [
+  {
+    firstName: "Noah",
+    lastName: "M.",
+    country: "Switzerland",
+    continent: "Europe",
+    age: 19,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Maia",
+    lastName: "S.",
+    country: "Tahiti",
+    continent: "Oceania",
+    age: 28,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Shufen",
+    lastName: "L.",
+    country: "Taiwan",
+    continent: "Asia",
+    age: 35,
+    language: "HTML",
+  },
+  {
+    firstName: "Sumayah",
+    lastName: "M.",
+    country: "Tajikistan",
+    continent: "Asia",
+    age: 30,
+    language: "CSS",
+  },
+];
+
+var list2 = [
+  {
+    firstName: "Oliver",
+    lastName: "Q.",
+    country: "Australia",
+    continent: "Oceania",
+    age: 19,
+    language: "HTML",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "R.",
+    country: "Austria",
+    continent: "Europe",
+    age: 89,
+    language: "HTML",
+  },
+];
+
+countDevelopers(list1);
+countDevelopers(list2);
