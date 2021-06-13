3fcb97fa73d6fb39fd18971788e08d22ad5c45a1
WON-DER <wonderfilledyt@gmail.com>
2021-06-12 21:06:42 -0700

    isRubyComing.js

---
+++ 6-16-21/isRubyComing.js
@@ -0,0 +1,54 @@
+// https://www.codewars.com/kata/5827acd5f524dd029d0005a4/train/javascript
+
+function isRubyComing(list) {
+  return list.filter((a) => a.language === "Ruby").length > 0;
+}
+
+var list1 = [
+  {
+    firstName: "Sofia",
+    lastName: "I.",
+    country: "Argentina",
+    continent: "Americas",
+    age: 35,
+    language: "Java",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "X.",
+    country: "Croatia",
+    continent: "Europe",
+    age: 35,
+    language: "Python",
+  },
+  {
+    firstName: "Madison",
+    lastName: "U.",
+    country: "United States",
+    continent: "Americas",
+    age: 32,
+    language: "Ruby",
+  },
+];
+
+var list2 = [
+  {
+    firstName: "Sofia",
+    lastName: "I.",
+    country: "Argentina",
+    continent: "Americas",
+    age: 35,
+    language: "Java",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "X.",
+    country: "Croatia",
+    continent: "Europe",
+    age: 35,
+    language: "Python",
+  },
+];
+
+isRubyComing(list1);
+isRubyComing(list2);
