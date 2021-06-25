1160c826a393256236cbafba040401eb65b76302
WON-DER <wonderfilledyt@gmail.com>
2021-06-25 09:46:44 -0700

    added containsDuplicate.js

---
+++ LC/containsDuplicate.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+var containsDuplicate = function (nums) {
+  const newNums = nums.reduce((acc, curr) => {
+    if (acc[curr]) {
+      acc[curr]++;
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});
+  return Object.entries(newNums).length === nums.length ? false : true;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
