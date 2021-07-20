025b49e91a077a74523259d60b8c35207a4e1596
WON-DER <wonderfilledyt@gmail.com>
2021-07-20 08:22:25 -0700

    added second function in highestScoringWord.js

--- 7-19-21/highestScoringWord.js
+++ 7-19-21/highestScoringWord.js
@@ -16,6 +16,19 @@ function high(x) {
   return x.split(" ")[copy.indexOf(Math.max(...copy))];
 }
 
+// function high(x) {
+//   const alphabet = " abcdefghijklmnopqrstuvwxyz";
+//   const score = (word) =>
+//     word.split("").reduce((a, c) => (a += alphabet.indexOf(c)), 0);
+//   let highestScore = 0;
+//   let word = ''
+//   x.split(" ").forEach(
+//     (word) =>
+//       (highestScore = score(word) > highestScore ? score(word) : highestScore)
+//   );
+//   return highestScore;
+// }
+
 console.log(high("man i need a taxi up to ubud"), "taxi");
 console.log(high("what time are we climbing up the volcano"), "volcano");
 console.log(high("take me to semynak"), "semynak");
