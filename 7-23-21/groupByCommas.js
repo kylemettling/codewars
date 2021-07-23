0e5d457760a83e1a281ca7f6f60e4715a015f9bb
kylemettling <58491427+kylemettling@users.noreply.github.com>
2021-07-23 10:16:14 -0700

    added groupByCommas.js

---
+++ 7-23-21/groupByCommas.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5274e122fc75c0943d000148/train/javascript
+
+function groupByCommas(n) {
+  return n.toLocaleString();
+}
+
+console.log(groupByCommas(1), "1");
+console.log(groupByCommas(10), "10");
+console.log(groupByCommas(100), "100");
+console.log(groupByCommas(1000), "1,000");
+console.log(groupByCommas(10000), "10,000");
+console.log(groupByCommas(100000), "100,000");
+console.log(groupByCommas(1000000), "1,000,000");
+console.log(groupByCommas(35235235), "35,235,235");
