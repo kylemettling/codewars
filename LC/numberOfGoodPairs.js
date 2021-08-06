29acd69108d166cee3447b5fc6e6756a6fcfff90
WON-DER <wonderfilledyt@gmail.com>
2021-08-06 14:51:16 -0700

    added numberOfGoodPairs.js

---
+++ LC/numberOfGoodPairs.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/number-of-good-pairs/
+
+var numIdenticalPairs = function (nums) {
+    const counts = nums.reduce((allPairs, curr, i) => {
+        
+    })
+};
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
+console.log(numIdenticalPairs([1, 1, 1, 1]));
+console.log(numIdenticalPairs([1, 2, 3]));
