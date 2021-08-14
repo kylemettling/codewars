4a3b5b47ae6e6f48832f4f4d83c7ca3a2cc96f94
WON-DER <wonderfilledyt@gmail.com>
2021-08-14 10:36:18 -0700

    added tests to fib.js

--- LC/fib.js
+++ LC/fib.js
@@ -5,3 +5,7 @@ const fibNum = (n) => (n < 2 ? n : fibNum(n - 1) + fibNum(n - 2));
 console.log(fibNum(2));
 console.log(fibNum(14));
 console.log(fibNum(8));
+console.log(fibNum(3));
+console.log(fibNum(0));
+console.log(fibNum(7));
+console.log(fibNum(13));
