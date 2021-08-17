52efe232f2b62fdc96bf9dd38dc2376ea370fc13
WON-DER <wonderfilledyt@gmail.com>
2021-08-17 15:14:40 -0700

    added test case to fib.js

--- LC/fib.js
+++ LC/fib.js
@@ -10,3 +10,8 @@ console.log(fibNum(0));
 console.log(fibNum(7));
 console.log(fibNum(13));
 console.log(fibNum(1));
+
+// function fibNum(n) {
+//     if(n < 2) {return n}
+//     return fibNum(n - 1) + fibNum(n - 2)
+// }
